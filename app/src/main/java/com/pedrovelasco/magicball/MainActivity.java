d350f51a467f483572669cdda4e1d4dc2cb19844
Pedro Velasco <pedro@pedro.com>
2015-12-18 14:29:13 +0100

    Version 1.1
    - Shake input

--- app/src/main/java/com/pedrovelasco/magicball/MainActivity.java
+++ app/src/main/java/com/pedrovelasco/magicball/MainActivity.java
@@ -1,10 +1,15 @@
 package com.pedrovelasco.magicball;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,18 +17,64 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+    private ShakeDetector mShakeDetector;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    */
+
+    static Context context;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        context = this;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
+         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new MainActivityFragment())
                     .commit();
         }
+        /*
+        mSensorManager = (SensorManager)  getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener() {
+            @Override
+            public void onShake() {
+                FragmentManager fm = getSupportFragmentManager();
 
+                //if you added fragment via layout xml
+                MainActivityFragment fragment
+                        = (MainActivityFragment)fm.findFragmentById(R.id.container);
+                fragment.predictionAction();
+                Log.d("mv","SAKET");
+            }
+        });
+
+        */
     }
+
+
+
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+    */
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/pedrovelasco/magicball/MainActivityFragment.java
+++ app/src/main/java/com/pedrovelasco/magicball/MainActivityFragment.java
@@ -1,13 +1,19 @@
 package com.pedrovelasco.magicball;
 
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -15,38 +21,91 @@ import android.widget.Toast;
 public class MainActivityFragment extends Fragment {
 
      static TextView text;
+    static ImageView bg;
+    View rootView;
+
+
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        setButtonListener(rootView);
 
-        return rootView;
+    return rootView;
     }
 
-    /**
-     * method that add listener to the button prediction
-     * @param rootView
-     *
-     */
-    private void setButtonListener(View rootView) {
 
-        text = (TextView) rootView.findViewById(R.id.textView);
-        Button button = (Button) rootView.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+    private ShakeDetector mShakeDetector;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+
+
+
+        mSensorManager = (SensorManager)  ((MainActivity)getActivity()).getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+        mShakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener() {
             @Override
-            public void onClick(View v) {
-
-
-                text.setText(MakePrediction.getPrediction(getContext()));
-
-
-
-
+            public void onShake() {
+            predictionAction();
             }
         });
+
+
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
+
+
+
+
+    public void predictionAction() {
+
+        text = (TextView) rootView.findViewById(R.id.textView);
+        bg = (ImageView) rootView.findViewById(R.id.imageView);
+
+                text.setText(MakePrediction.getPrediction(getContext()));
+                animatedball();
+
+
+    }
+
+
+
+
+
+
+    public void animatedball(){
+        bg.setBackgroundResource(R.drawable.ball_animation);
+
+
+        AnimationDrawable frameAnimation = (AnimationDrawable) bg.getBackground();
+        if(frameAnimation.isRunning()){
+            frameAnimation.stop();
+        }
+        frameAnimation.start();
     }
 
 
